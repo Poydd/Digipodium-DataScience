1779b37cbf9b8b21448864dbc804c5f92f97c8b3
Vidushi <vidushibajpai82@gmail.com>
2023-07-02 12:36:22 +0530

    conditional and turtle

---
+++ t1.py
@@ -0,0 +1,22 @@
+from turtle import *
+speed ('slowest')
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+fd(150)
+rt(60)
+
+hideturtle()
+mainloop() # hold the window open
+
+
+for 1 in range(10):
+    fd (100)
+    rt(360/10)
