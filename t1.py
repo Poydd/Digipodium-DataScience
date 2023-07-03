e2fbd898132721070120594b6d15b26343466761
Vidushi <vidushibajpai82@gmail.com>
2023-07-03 12:26:07 +0530

    loops and turtle

--- t1.py
+++ t1.py
@@ -13,10 +13,19 @@ rt(60)
 fd(150)
 rt(60)
 
+for range in
+
+
+
+
+
+
+
+
 hideturtle()
 mainloop() # hold the window open
 
 
-for 1 in range(10):
-    fd (100)
-    rt(360/10)
+
+
+
---
+++ t2.py
@@ -0,0 +1,22 @@
+from turtle import *
+pencolor('rainbow')
+pensize(2)
+bgcolor('black')
+speed('fastest')
+
+for i in range(5):
+    fd(100)
+    rt(360/5)
+    for i in range (5):
+        fd(50)
+        rt(360/5)
+    for i in range (5):
+        bk(50)
+        lt(360/5)
+    for i in range (5):
+        bk(50)
+        lt(360/5)
+
+
+hideturtle()
+mainloop()
