ff87fa720c1be73f8825562eec643e25863910f3
Vidushi <vidushibajpai82@gmail.com>
2023-07-27 20:41:14 +0530

    games done

--- game1.py
+++ game1.py
@@ -1,68 +1,62 @@
 import pgzrun
 from random import randint
-
 # screen size
 WIDTH = 1000
 HEIGHT = 500
-
 # objects
-p = Actor('hero')                                 # actor is class
+p = Actor('hero')
 e = Actor('enemy')
 c = Actor('friut')
-
 # configs
 c.x = randint(50, WIDTH-50)
 c.y = randint(50, HEIGHT-50)
-p.POS = (WIDTH/2, HEIGHT/2)
+p.pos = (WIDTH/2, HEIGHT/2)  
 e.pos = (-100, HEIGHT/2)
 ps = 5
 es = 2
 score = 0
-
-# player control
 def player_control():
-  if keyboard.left:
-    p.x -=ps
-  if keyboard.right:
-            p.x +=ps
-  if keyboard.up:
-            p.y -=ps
-        if keyboard.down:
-            p.y +=ps
-        if keyboard.space:
-            p.angle +=ps
-
-  randint(50,WIDTH-50)
-  y = randint(50,HEIGHT-50)
+    # player control
+    if keyboard.left:
+        p.x -= ps
+    if keyboard.right:
+        p.x += ps
+    if keyboard.up:
+        p.y -= ps
+    if keyboard.down:
+        p.y += ps
+    if keyboard.space:
+        p.angle += ps 
 def enemy_tracking():
-  if p.x > e.x:
-    e.x += 1
-  if p.x < e.x:
-    e.x -= 1
-  if p.y > e.y:
-    e.y += 1
-  if p.y < e.y:
-    e.y -= 1
-def friut_eating():
-  if p.colliderect(c):
-    c.x = randint(50,WIDTH-50)
-    c.y = randint(50,HEIGHT-50)
-    score += 10
-    sounds.clap.play()
-
-# drawing on screen
+    # enemy tracks player
+    if p.x > e.x:
+        e.x += es
+    if p.x < e.x:
+        e.x -= es 
+    if p.y > e.y:
+        e.y += es
+    if p.y < e.y:
+        e.y -= es
+    print(f'player {p.pos} enemy {e.pos}')
+    if e.colliderect(p):
+        exit()
+def fruit_eating():
+    # fruit collision
+    global score
+    if p.colliderect(c):
+        c.x = randint(50, WIDTH-50)
+        c.y = randint(50, HEIGHT-50)
+        score += 10
+        sounds.clap.play()
 def draw():
     screen.clear()
     p.draw()
     e.draw()
     c.draw()
-    screen.draw.text(f'score: {score}',(10,10),color='white')
-
+    screen.draw.text(f'Score: {score}', (10, 10), color='white')
 def update(dt):
-   player_control()
-   enemy_tracking()
-   friut_eating()
-
-
+    player_control()
+    enemy_tracking()
+    fruit_eating()
 # game loop
 pgzrun.go()
